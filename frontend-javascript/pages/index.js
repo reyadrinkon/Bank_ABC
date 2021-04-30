a3a66456a54967a0427b0d51cb5c216a1e335b7f
Reyad Hasan Rinkon <45186655+rinkon005@users.noreply.github.com>
2021-04-30 07:29:10 +0600

    Update index.js

--- frontend-javascript/pages/index.js
+++ frontend-javascript/pages/index.js
@@ -11,55 +11,44 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to BANK_ABC
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          A digital Bank working on Blockchain Technology
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="http://localhost:3001/register" className={styles.card}>
+            <h3>Create Account  &rarr;</h3>
+            <p>To open a Bank account one need to have an valid NID card of Banglaedsh</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="http://localhost:3001/deposit" className={styles.card}>
+            <h3>Desposit in an account &rarr;</h3>
+            <p>To deposit in an account you only have to know the NID number of that account holder</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="http://localhost:3001/withdraw"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>Withdraw Money from account &rarr;</h3>
+            <p>You have to provide Account holder NID and his password.</p>
           </a>
+          <a
+            href="http://localhost:3001/send"
+            className={styles.card}
+          >
+            <h3>Send Money from one account to another &rarr;</h3>
+            <p>You have to provide sender and reciever NID and sender's password.</p>
+          </a>
+         
+
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+
     </div>
   )
 }
