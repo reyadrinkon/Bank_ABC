aeaed89d376551f5011d7d7047c3955abfe134d7
Reyad Hasan Rinkon <45186655+rinkon005@users.noreply.github.com>
2021-05-07 06:22:21 +0600

    Update _app.js

--- frontend-javascript/pages/_app.js
+++ frontend-javascript/pages/_app.js
@@ -1,6 +1,5 @@
-import '../styles/globals.css';
 import 'antd/dist/antd.css';
-
+import '../styles/globals.css';
 import { Layout, Menu } from 'antd';
 import {
   AppstoreOutlined,
@@ -12,35 +11,56 @@ import {
   UploadOutlined,
   VideoCameraOutlined,
 } from '@ant-design/icons';
-import LeftsideBar from "../components/LeftsideBar";
+import {useRouter} from 'next/router';
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
 
 
 function MyApp({ Component, pageProps }) {
-  //return
-  return (
-      <Layout>
+  const router =useRouter();
+  //return <Component {...pageProps} />
+  return  <Layout>
+  <Sider
+    style={{
+      overflow: 'auto',
+      height: '100vh',
+      position: 'fixed',
+      left: 0,
+    }}
+  >
+    <h2 className="logo">BANK_ABC</h2>
+    <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+      <Menu.Item key="1" icon={<UserOutlined />} onClick={()=> router.push('/register')}>
+        Create Account
+      </Menu.Item>
+      <Menu.Item key="3" icon={<UploadOutlined />} onClick={()=> router.push('/deposit')}>
+        Deposit Money
+      </Menu.Item>
+      <Menu.Item key="6" icon={<AppstoreOutlined />} onClick={()=> router.push('/withdraw')}>
+        Withdraw Money 
+      </Menu.Item>
+      <Menu.Item key="7" icon={<TeamOutlined />} onClick={()=> router.push('/send')}>
+        Send Money
+      </Menu.Item>
+      
+     
+    </Menu>
+  </Sider>
+  <Layout className="site-layout" style={{ marginLeft: 200 }}>
+    <Header className="site-layout-background" style={{ padding: 0 }} />
+    <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+      <div className="site-layout-background" style={{ padding: 24}}>
+        {/*--------*/}
+        <Component{...pageProps}/>
+        {/*--------*/}
 
-        <LeftsideBar/>
-    <Layout className="site-layout" style={{ marginLeft: 200 }}>
-      <Header className="site-layout-background" style={{ padding: 0 }} />
-      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-        <div className="site-layout-background" style={{ padding: 12,}}>
-          {/*-----------*/}
-          <Component {...pageProps} />
-          {/*-----------*/}
-
-        </div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-    </Layout>
+      </div>
+    </Content>
   </Layout>
-  )
-
+</Layout>
 }
 
 export default MyApp
-
+/////////////////////////////////
 
 
